7bee85f10e8124a344d32989ae27dd5a2a75a9bd
svetik_lion <svetlana.lvovskaya@gmail.com>
2023-06-14 00:33:56 +0500

    Add logo

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var logo: UIImageView = {
+        let logoView = UIImageView()
+        logoView.image = UIImage(named: "Uni.jpeg")
+        return logoView
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(logo)
+        setupConstraints()
+        
+    }
+    
+    private func setupConstraints() {
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            logo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            logo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logo.widthAnchor.constraint(equalToConstant: view.frame.size.width / 1.5),
+            logo.heightAnchor.constraint(equalToConstant: view.frame.size.width / 2.7),
+            
+        ])
     }
 
 
