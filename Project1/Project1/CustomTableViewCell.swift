0c15963413897158b56d79a48607475fe92354bd
svetik_lion <svetlana.lvovskaya@gmail.com>
2023-06-17 00:26:04 +0500

    fckng round

--- Project1/Project1/CustomTableViewCell.swift
+++ Project1/Project1/CustomTableViewCell.swift
@@ -2,16 +2,22 @@ import UIKit
 
 
 class CustomTableViewCell: UITableViewCell {
-    var circleView: UIView = {
-        let circleView = UIView()
-        circleView.backgroundColor = .systemPink
-        circleView.layer.cornerRadius = 25
-        return circleView
+    
+    var circleImageView: UIImageView = {
+        let circleImageView = UIImageView(frame: CGRectMake(0, 0, 100, 100))
+        circleImageView.image = UIImage(named: "Uni.jpeg")
+        circleImageView.layer.cornerRadius =  circleImageView.frame.size.width / 2.8
+        circleImageView.layer.borderWidth = 1
+        circleImageView.layer.masksToBounds = false
+        circleImageView.layer.borderColor = UIColor.systemPink.cgColor
+        circleImageView.clipsToBounds = true
+        return circleImageView
     }()
     
     var smallTextLabel: UILabel = {
         let smallTextLabel = UILabel()
         smallTextLabel.backgroundColor = .systemPurple
+        smallTextLabel.textColor = .white
         smallTextLabel.textAlignment = .center
         smallTextLabel.text = "Имя Фамилия"
         return smallTextLabel
@@ -20,6 +26,7 @@ class CustomTableViewCell: UITableViewCell {
     var bigTextLabel: UILabel = {
         let bigTextLabel = UILabel()
         bigTextLabel.backgroundColor = .systemPurple
+        bigTextLabel.textColor = .white
         bigTextLabel.textAlignment = .center
         bigTextLabel.text = "Мало кто поймет, но кто поймет, тот - мало кто"
         bigTextLabel.numberOfLines = 4
@@ -27,22 +34,22 @@ class CustomTableViewCell: UITableViewCell {
     }()
     
     func setupConstraints() {
-        circleView.translatesAutoresizingMaskIntoConstraints = false
+        circleImageView.translatesAutoresizingMaskIntoConstraints = false
         smallTextLabel.translatesAutoresizingMaskIntoConstraints = false
         bigTextLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            circleView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            circleView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
-            circleView.heightAnchor.constraint(equalToConstant: 50),
-            circleView.widthAnchor.constraint(equalTo: circleView.heightAnchor),
+            circleImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            circleImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            circleImageView.heightAnchor.constraint(equalToConstant: 70),
+            circleImageView.widthAnchor.constraint(equalToConstant: 70),
             
             smallTextLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
-            smallTextLabel.leadingAnchor.constraint(equalTo: circleView.trailingAnchor, constant: 30),
+            smallTextLabel.leadingAnchor.constraint(equalTo: circleImageView.trailingAnchor, constant: 30),
             smallTextLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -30),
             
             bigTextLabel.topAnchor.constraint(equalTo: smallTextLabel.topAnchor, constant: 30),
-            bigTextLabel.leadingAnchor.constraint(equalTo: circleView.trailingAnchor, constant: 10),
+            bigTextLabel.leadingAnchor.constraint(equalTo: circleImageView.trailingAnchor, constant: 10),
             bigTextLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
             bigTextLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
             bigTextLabel.heightAnchor.constraint(equalToConstant: 100),
@@ -50,7 +57,7 @@ class CustomTableViewCell: UITableViewCell {
     }
     
     func setupViews() {
-        contentView.addSubview(circleView)
+        contentView.addSubview(circleImageView)
         contentView.addSubview(smallTextLabel)
         contentView.addSubview(bigTextLabel)
         setupConstraints()
