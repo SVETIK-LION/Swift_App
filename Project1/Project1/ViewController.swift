2d5bd4a1d9324d0dbe2e666ba4e3cc01dc3e9b36
svetik_lion <svetlana.lvovskaya@gmail.com>
2023-06-25 20:19:14 +0500

    ?

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -5,15 +5,17 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     private var session = URLSession.shared
     
+    // 1) Добавила WKWebView
+    private lazy var webView: WKWebView = {
+        let webView = WKWebView()
+        webView.navigationDelegate = self
+        return webView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        addButtonTarget()
         view.backgroundColor = .white
-        view.addSubview(logoView)
-        view.addSubview(authView)
-        view.addSubview(userView)
-        view.addSubview(passwordView)
-        view.addSubview(welcomeButton)
+        view.addSubview(webView) // Добавила webView на главное view
         
         let url: URL? = URL(string: "https://oauth.vk.com/authorize?client_id=1&redirect_uri=http://example.com/callback&scope=12&display=mobile")
         session.dataTask(with: url!) { (data, _, error) in
@@ -25,108 +27,29 @@ class ViewController: UIViewController, WKNavigationDelegate {
                 print(error)
             }
         }.resume()
-        webView.load(URLRequest(url: url!))
+        webView.load(URLRequest(url: url!)) // Метод загрузки
         
         setupConstraints()
-        
     }
     
-    private lazy var webView: WKWebView = {
-        let webView = WKWebView()
-        webView.navigationDelegate = self
-        return webView
-    }()
+    // 1) Что за метод func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void)  ? Как его реализовывать и зачем?
     
-    var logoView: UIImageView = {
-        let logoView = UIImageView()
-        logoView.image = UIImage(named: "Uni.jpeg")
-        return logoView
-    }()
+    // 1) Приложение создала, что делать с ID приложения? 51685230.
     
-    var authView: UILabel = {
-        let authView = UILabel()
-        authView.backgroundColor = .purple
-        authView.text = "authorization".uppercased()
-        authView.font = .systemFont(ofSize: 24)
-        authView.textAlignment = .center
-        authView.textColor = .white
-        return authView
-    }()
-
-    var userView: UITextField = {
-        let userView = UITextField()
-        userView.borderStyle = .roundedRect
-        userView.backgroundColor = .systemPink
-        userView.placeholder = "Username"
-        userView.textColor = .white
-        userView.textAlignment = .center
-        userView.font = .systemFont(ofSize: 24)
-        
-        return userView
-    }()
+    // 2) Вообще не поняла. Наверное логичнее будет авторизацию делать ещ едо перехода к сообщениям, то есть во ViewController.
     
-    var passwordView: UITextField = {
-        let passwordView = UITextField()
-        passwordView.borderStyle = .roundedRect
-        passwordView.backgroundColor = .systemPink
-        passwordView.placeholder = "password"
-        passwordView.textColor = .white
-        passwordView.textAlignment = .center
-        passwordView.font = .systemFont(ofSize: 24)
-        passwordView.isSecureTextEntry = true
-        return passwordView
-    }()
+    // 3) Возможно стоит использовать getConversations - Возвращает список бесед пользователя.
     
-    var welcomeButton: UIButton = {
-        let welcomeButton = UIButton()
-        welcomeButton.backgroundColor = .purple
-        welcomeButton.setTitle("log in".uppercased(), for: .normal)
-        welcomeButton.setTitleColor(.white, for: .normal)
-        welcomeButton.setTitleColor(.systemYellow, for: .highlighted)
-        return welcomeButton
-    }()
+    // 4) ...
     
     private func setupConstraints() {
-        logoView.translatesAutoresizingMaskIntoConstraints = false
-        authView.translatesAutoresizingMaskIntoConstraints = false
-        userView.translatesAutoresizingMaskIntoConstraints = false
-        passwordView.translatesAutoresizingMaskIntoConstraints = false
-        welcomeButton.translatesAutoresizingMaskIntoConstraints = false
+        webView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            logoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            logoView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            logoView.widthAnchor.constraint(equalToConstant: view.frame.size.width / 1.5),
-            logoView.heightAnchor.constraint(equalToConstant: view.frame.size.width / 2.7),
-            
-            authView.topAnchor.constraint(equalTo: logoView.bottomAnchor, constant: 40),
-            authView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            authView.widthAnchor.constraint(equalToConstant: view.frame.size.width / 2),
-            authView.heightAnchor.constraint(equalToConstant: view.frame.size.width / 5),
-            
-            userView.topAnchor.constraint(equalTo: authView.bottomAnchor, constant: 60),
-            userView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            userView.widthAnchor.constraint(equalToConstant: view.frame.size.width / 1.5),
-            userView.heightAnchor.constraint(equalToConstant: view.frame.size.width / 7),
-            
-            passwordView.topAnchor.constraint(equalTo: userView.bottomAnchor, constant: 30),
-            passwordView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            passwordView.widthAnchor.constraint(equalToConstant: view.frame.size.width / 1.5),
-            passwordView.heightAnchor.constraint(equalToConstant: view.frame.size.width / 7),
-            
-            welcomeButton.topAnchor.constraint(equalTo: passwordView.bottomAnchor, constant: 60),
-            welcomeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            welcomeButton.widthAnchor.constraint(equalToConstant: view.frame.size.width / 2),
-            welcomeButton.heightAnchor.constraint(equalToConstant: view.frame.size.width / 5),
-            
+            webView.topAnchor.constraint(equalTo: view.topAnchor,  constant: 50),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 50),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
         ])
-    }
-    
-    private func addButtonTarget() {
-        welcomeButton.addTarget(self, action: #selector(tapWelcomeButton), for: .touchUpInside)
-    }
-    
-    @objc func tapWelcomeButton() {
-        navigationController?.pushViewController(TableViewController(), animated: true)
     }
 }
